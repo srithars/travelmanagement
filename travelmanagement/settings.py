883342b558c3f9bf67f15f46d0c52b17b0d2fc7e
srithars <64460687+srithars@users.noreply.github.com>
2025-03-17 08:14:39 +0530

    Update settings.py

--- travelmanagement/settings.py
+++ travelmanagement/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         "USER": "postgres",
         "USER": "postgres",
         "PASSWORD": "adminadmin",
-        "HOST": "database-1.crc2ucwuugqr.ap-south-1.rds.amazonaws.com",
+        "HOST": "database-1.c904si8gkwjp.ap-south-1.rds.amazonaws.com",
         "PORT": "5432",
     }
 }
